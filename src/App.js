7c1b262834a189f64d96f49d98cd74ee38ecdcc0
grvrjt <gaurav1993rajput@gmail.com>
2021-12-03 09:18:45 +0530

    Feat: setState use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Message from "./components/Message";
-
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
@@ -23,6 +23,7 @@ function App() {
       <Welcome name="Saurav" />
       <Welcome name="Kaurav" />
       <Message />
+      <Counter />
     </div>
   );
 }
